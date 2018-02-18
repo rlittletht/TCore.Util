3ab09d3edb90d4daabd89907076ed608fe3302b9
rlittletht <rlittle@thetasoft.com>
2018-02-17 16:08:42 -0800

    add filename support

---
+++ filename.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TCore.Util
+{
+    public class Filename
+    {
+        /*----------------------------------------------------------------------------
+        	%%Function: SBuildTempFilename
+        	%%Qualified: TCore.Util.Filename.SBuildTempFilename
+        	%%Contact: rlittle
+        	
+        ----------------------------------------------------------------------------*/
+        public static string SBuildTempFilename(string sBaseName, string sExt)
+        {
+            if (sBaseName == null)
+                sBaseName = "";
+
+            if (sExt == null)
+                return String.Format("{0}\\{1}{2}", Environment.GetEnvironmentVariable("Temp"),
+                                     sBaseName,
+                                     System.Guid.NewGuid().ToString());
+            return String.Format("{0}\\{1}{2}.{3}", Environment.GetEnvironmentVariable("Temp"),
+                                     sBaseName,
+                                     System.Guid.NewGuid().ToString(),
+                                     sExt);
+        }
+
+
+    }
+}
